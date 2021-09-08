9667e0744d0bed567e8a9c14bb74563a82b15d40
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-08 09:24:54 +0100

    Refactoring

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ var studente = {
 };
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for(var key in studente){
-    console.log(studente[key]);
+    console.log(key); // stampa le proprietà
+    console.log(studente[key]); // stampa i valori
+    console.log(`${key} => ${studente[key]}`) // stampa entrambi insieme
 }
 // Creare un array di oggetti di studenti.
 var studenti = [
@@ -28,19 +30,18 @@ var studenti = [
 ]
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 
+// console.log(studenti[1].nome); //estrarre valore di un singolo oggetto
+
 for(i=0; i < studenti.length; i++){
     console.log(`Nome: ${studenti[i]["nome"]}`);
     console.log(`Cognome: ${studenti[i]["cognome"]}`);
 }
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-for(i=0; i < 3; i++){
-    var nuovoStudente = {
-        "nome" : prompt("Inserisci il nome del nuovo studente"),
-        "cognome" : prompt("Inserisci il cognome del nuovo studente"),
-        "eta" : parseInt(prompt("Inserisci l'età del nuovo studente"))
-    };
-    studenti.push(nuovoStudente);
-}
+var nuovoStudente = {
+    "nome" : prompt("Inserisci il nome del nuovo studente"),
+    "cognome" : prompt("Inserisci il cognome del nuovo studente"),
+    "eta" : parseInt(prompt("Inserisci l'età del nuovo studente"))
+};
+studenti.push(nuovoStudente);
 
-console.log(studenti);
